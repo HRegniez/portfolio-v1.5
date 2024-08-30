2578dcd8dfc1d8c6ae6cc1e80000d968d7954fda
HRegniez <henriregniez124@gmail.com>
2024-08-30 17:44:00 +0200

    update

--- app/_sections/competence-section.tsx
+++ app/_sections/competence-section.tsx
@@ -1,8 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Image from "next/image"
 import IconCloud from "@/components/icon-cloud"
-import CompetencesParcours from "../../components/competences-parcours"
-
+import CompetencesParcours from "@/components/competences-parcours"
 
 
 
